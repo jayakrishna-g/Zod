ee069a0af164c81a0b05efd6f8bd8f1de9d8f952
jayakrishna-g <gandhamallajayakrishna@outlook.com>
2020-11-27 18:51:18 +0530

    initial commit

---
+++ apps/zod-rating/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'zod-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'zod-rating';
+}
