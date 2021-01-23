ad0365186a996885520afcf9efedeb72c8a3e035
jayakrishna-g <gandhamallajayakrishna@hotmail.com>
2021-01-24 01:03:59 +0530

    Project Setup: Heroku Deployment

---
+++ startScript.sh
@@ -0,0 +1,27 @@
+echo -e "Starting Postinstall Script\n\n\n"
+cd RestApi
+echo -e "***********************In RestApi***********************"
+npm i
+echo -e "***********************Completed installing Packages in RestApi***********************\n\n\n"
+cd ..
+echo -e "*******************************Back to Root***********************************\n\n\n"
+cd Scrappers
+echo -e "***********************In Scrappers***********************\n\n\n"
+npm i
+echo -e "***********************Completed installing Packages in Scrappers***********************\n\n\n"
+cd ..
+echo -e "*******************************Back to Root***********************************\n\n\n"
+cd UI
+echo -e "***********************In UI***********************\n\n\n"
+echo -e "***********************Installing angular***********************\n\n\n"
+
+npm i @angular/cli
+
+echo -e "***********************Installed angular***********************\n\n\n"
+npm i
+
+echo -e "***********************Completed installing Packages in UI***********************\n\n\n"
+npm run build
+echo -e "***********************Angular Build Completed***********************\n\n\n"
+cd ..
+echo -e "*******************************Back to Root***********************************\n\n\n"
